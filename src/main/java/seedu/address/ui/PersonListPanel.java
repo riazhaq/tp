5e3d8fdd9b1031be2b5b3ca1400029b3a6463381
riazhaq <riazhaq03@gmail.com>
2026-04-06 23:58:19 +0800

    remove problematic tests

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
 /**
- * Panel containing the list of persons.
+ * Panel containing list of persons.
  */
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
